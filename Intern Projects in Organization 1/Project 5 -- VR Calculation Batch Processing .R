d4dd40efe9d2e29e14ca50e69a9594a36afa3f1a
jiansfoggy <jian_sun@gwu.edu>
2018-01-31 21:50:25 -0500

    Project Details
    Batch Processing Code for R

--- Intern Projects in Organization 1/Project 5 -- VR Calculation Batch Processing .R
+++ Intern Projects in Organization 1/Project 5 -- VR Calculation Batch Processing .R
@@ -11,6 +11,5 @@ step1=sqldf("select * from data_target natural join coefficient where coefficien
 step2=sqldf("select * from step1 natural join test2 where step1.POST=test2.POST")
 #fdata=sqldf("select Coefficient from coefficient union select * from data_target where coefficient.AA=data_target.AA")
 
-
 write.xlsx(step2, "//dcna_cifs.peacecorps.gov/users/jsun/My Documents/R/Project 5/Project5.xlsx")
 #Inv_deadline = -3.560086 + 1.475775*VR + AA +1.327504*Fre
