d0e6d70df9937fe91f1fa098924be36e037720c7
jiansfoggy <jian_sun@gwu.edu>
2021-01-31 21:12:09 -0700

    Update strt_trn.sh

--- Python/EffDet/strt_trn.sh
+++ Python/EffDet/strt_trn.sh
@@ -6,4 +6,4 @@
  
 #CUDA_VISIBLE_DEVICES=1,2 python3 train.py --image_size 416 --batch_size 16 --device 1,2 --num_epochs 3 --data_path /media/data3/jian/Text_Detection/YOLOV3/ --log_path ./log
  
-python3 train.py --image_size 512 --batch_size 16 --num_epochs 2 --data_path /media/data3/jian/Text_Detection/YOLOV3/ --log_path ./log
+python3 train.py --image_size 512 --batch_size 16 --num_epochs 2 --data_path ./ --log_path ./log
