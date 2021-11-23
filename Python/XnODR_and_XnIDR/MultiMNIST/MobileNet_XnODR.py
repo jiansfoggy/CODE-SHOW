a2ff985bb5dcd4fa8939913e7b84f1c7e4902a3b
jiansfoggy <jian_sun@gwu.edu>
2021-11-22 23:29:14 -0700

    Update MobileNet_XnODR.py

--- Python/XnODR_and_XnIDR/MultiMNIST/MobileNet_XnODR.py
+++ Python/XnODR_and_XnIDR/MultiMNIST/MobileNet_XnODR.py
@@ -10,7 +10,6 @@ from keras.datasets import mnist
 from keras.optimizers import Adam
 from keras.models import Sequential
 from keras.utils import np_utils
-# from keras.optimizers import SGD, Adam, RMSprop
 from tensorflow.python.framework import ops
 from keras.utils.vis_utils import plot_model
 from keras import callbacks, initializers, layers, models, constraints, optimizers
@@ -74,7 +73,6 @@ def binary_tanh(x):
     return 2 * round_through(_hard_sigmoid(x)) - 1
 
 def binarize(W, H=1):
-    # [-H, H] -> -H or H
     Wb = H * binary_tanh(W / H)
     return Wb
 
@@ -87,11 +85,7 @@ def xnorize(W, H=1., axis=None, keepdims=False):
     return Wa, Wb
 
 class Length(layers.Layer):
-    """
-    Compute the length of vectors. This is used to compute a Tensor that has the same shape with y_true in margin_loss
-    inputs: shape=[dim_1, ..., dim_{n-1}, dim_n]
-    output: shape=[dim_1, ..., dim_{n-1}]
-    """
+
     def call(self, inputs, **kwargs):
         return K.sqrt(K.sum(K.square(inputs), -1))
 
@@ -226,20 +220,13 @@ def preprocess_image_input(input_images):
 def MBL_Primary(x, dim_vector, n_channels, strides):
     
     x = DepthwiseConv2D(kernel_size = 3, strides = strides, padding = 'same')(x)
-    #x = BatchNormalization(epsilon=1e-6, momentum=0.9, axis=-1)(x)
     x = BatchNormalization()(x)
-    #x = ReLU()(x)
 
     x = Conv2D(filters = dim_vector*n_channels, kernel_size = 1, strides = 1)(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)    
-    #x = AvgPool2D (pool_size = 3, strides = 1, data_format='channels_last')(x)
-    #x = tf.nn.max_pool(x, ksize=[1, 7, 7, 1], strides=[1, 1, 1, 1], padding='VALID')
-    #x = tf.keras.layers.Dropout(0.5)(x)
     x = layers.Reshape(target_shape=[n_channels, dim_vector])(x)
     x = layers.Lambda(squash)(x)
-    #x = BatchNormalization(epsilon=1e-6, momentum=0.9, axis=-1)(x)
     x = BatchNormalization()(x)
-    #x = ReLU()(x)
     x = layers.advanced_activations.ReLU()(x)
     return x
 
@@ -254,11 +241,8 @@ def feature_extractor(inputs):
 def classifier(inputs):
     x = MBL_Primary(inputs, dim_vector=8, n_channels=128, strides = 1)
     XDR4 = XDR1_LPLayer(num_capsule=10, dim_vector=16, num_routing=3, name='XDR4')(x)
-    #Bn4 = layers.BatchNormalization(epsilon=epsilon, momentum=momentum, axis=channel_axis, name='Bn4')(XDR4)
     Bn4 = layers.BatchNormalization(name='Bn4')(XDR4)
-    #x = layers.Activation('relu')(Bn4)
     x = layers.advanced_activations.ReLU()(Bn4)
-    #x = tf.keras.layers.Dropout(.2)(x)
     x = Length(name='out_cpxn')(x)
     return x
 
@@ -443,34 +427,3 @@ print("Top5 Acc: ", top5)
 print('-'*50)
 print("The total validation time is: ", (after_T-before_T), " seconds.")
 print('-'*50)
-
-"""
-1
-
-FLOPs: 13372966;    Trainable params: 3714864
---------------------------------------------------
-Top1 Acc:  0.18085
-Top2 Acc:  0.974675
-Top5 Acc:  0.9995
---------------------------------------------------
-The total validation time is:  25.599448442459106  seconds.
---------------------------------------------------
-
-=======================
-Without Reconstruct
-=======================
-
-top1    = [86.40,86.42,86.51,86.15,86.09]
-top5    = [99.15,99.05,99.12,99.13,99.01]
-time_ls = [6819.390365600586,7252.462275266647,6946.476444482803,7026.700607538223,7025.691478967667]
-
-Standard Deviation
-Top1 86.31400000000001 0.1637803407005859
-Top5 99.092 0.053065996645686335
-Time 7014.144234371185 141.13432549923922
-
-ResNet-50
-1
---------------------------------------------------
-
-"""
